b2b10631bf47892023b5698757e6fe64eaddc1a2
Yvonne Yeh <code@yvonneyeh.com>
2021-06-08 10:47:25 -0700

    Create gameplay functions

---
+++ game.py
@@ -0,0 +1,22 @@
+import re
+import random
+import string
+from hats import hats
+
+# QUESTIONS LIST
+q_file = open("questions.txt", "r")
+content = q_file.read()
+question_list = content.split("\n")
+q_file.close()
+
+
+
+def play_game():
+    """ Initiate gameplay. """
+
+    pass
+
+
+
+if __name__ == '__main__':
+    play_game()
